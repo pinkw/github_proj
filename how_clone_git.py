a1e6ea20a30f55ba59ff42ba52100b504a9a1fb2
panyang <15926297114@163.com>
2016-10-27 21:09:48 +0800

    new test file

---
+++ how_clone_git.py
@@ -0,0 +1,9 @@
+# --encoding:utf-8 --
+#this project is cloned from github
+#I have done those steps as follows:
+#vcs-> checkout from version controll ->git ->add the github url
+
+print 'you have know how to clone project from github to local '
+
+# then try to push this file to github
+print 'just use git then add to respository'
